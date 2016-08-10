edcbd64f9cfa5b570465586caa0f5737e81357ee
checkkdahustle <checkkdahustle@fullsail.edu>
2016-08-09 21:33:04 -0400

    renamed file and added requirements

---
+++ adHubApp_v2/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('passport-local'), Strategy;
+var mongodb = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/')
