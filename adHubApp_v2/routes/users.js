ba8761af8fc0e0b865e0793f2f02f88fd2532bd2
checkkdahustle <checkkdahustle@fullsail.edu>
2016-08-13 23:48:55 -0400

    set var to natch form inputs

--- adHubApp_v2/routes/users.js
+++ adHubApp_v2/routes/users.js
@@ -11,4 +11,20 @@ router.get('/login', function(req, res){
 	res.render('login');
 });
 
+// register users
+router.post('/register', function(req, res){
+	// var fullName= req.body.fname + ' ' + req.body.lname;
+	var fname= req.body.fname;
+	var lname= req.body.lname;
+	var email= req.body.lname;
+	var dob= req.body.dob;
+	var gender= req.body.gender;
+	var username= req.body.username;
+	var passcode= req.body.passcode;
+	var passcode2= req.body.passcode2;
+	// console.log(fullName);
+
+
+});
+
 module.exports = router;
