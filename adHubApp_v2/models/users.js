476b996be60439f5227a97c1e693d01d836bbc89
checkkdahustle <checkkdahustle@fullsail.edu>
2016-08-14 02:11:53 -0400

    created 2 functions for username and password checkk

--- adHubApp_v2/models/users.js
+++ adHubApp_v2/models/users.js
@@ -27,9 +27,21 @@ var User = module.exports = mongoose.model('User', userSchema);
 
 module.exports.createUser = function(newUser, caLLback){
 	bcrypt.genSalt(10, function(err, salt) {
-	    bcrypt.hash(newUser.passcode, salt, function(err, hash) {
-	        newUser.passcode = hash;
-					newUser.save(caLLback);
-	    });
+		bcrypt.hash(newUser.passcode, salt, function(err, hash) {
+			newUser.passcode = hash;
+			newUser.save(caLLback);
+		});
+	});
+}
+
+module.exports.getUserByUsername = function (username, caLLback) {
+	var query ={ username: username};
+	User.findOne(query, caLLback);
+}
+
+module.exports.comparedPassword = function (candidatePassword, hash, caLLback) {
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+		if(err) throw err;
+		caLLback(null, isMatch);
 	});
 }
