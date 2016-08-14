c9514cee8e154d6579fd58675f0c35641264df30
checkkdahustle <checkkdahustle@fullsail.edu>
2016-08-14 01:01:38 -0400

    validation added

--- adHubApp_v2/routes/users.js
+++ adHubApp_v2/routes/users.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/users');
 
 // register
 router.get('/register', function(req, res){
@@ -16,7 +18,7 @@ router.post('/register', function(req, res){
 	// var fullName= req.body.fname + ' ' + req.body.lname;
 	var fname= req.body.fname;
 	var lname= req.body.lname;
-	var email= req.body.lname;
+	var email= req.body.email;
 	var dob= req.body.dob;
 	var gender= req.body.gender;
 	var username= req.body.username;
@@ -24,7 +26,40 @@ router.post('/register', function(req, res){
 	var passcode2= req.body.passcode2;
 	// console.log(fullName);
 
+	// Validation
+	// req.checkBody('fullName', 'Full Name is required').notEmpty();
+	req.checkBody('fname', 'First Name required').notEmpty();
+	req.checkBody('lname', 'Last Name required').notEmpty();
+	req.checkBody('email', 'Email required').notEmpty();
+	req.checkBody('email', 'Email not valid').isEmail();
+	req.checkBody('username', 'Username required').notEmpty();
+	req.checkBody('passcode', 'Password required').notEmpty();
+	req.checkBody('passcode2', 'Password do not match').equals(req.body.passcode);
 
+
+	var errors = req.validationErrors();
+
+	if (errors) {
+		res.render('register', {
+			errors:errors
+		});
+	} else {
+		var newUser = new User({
+			fname:fname,
+			lname:lname,
+			email:email,
+			username:username,
+			passcode:passcode
+		});
+
+		User.createUser(newUser, function (err, user) {
+			if(err) throw err;
+			console.log(user);
+		});
+		req.flash('success_msg', 'Registration Successful and you can now login');
+
+		res.redirect('../users/login');
+	}
 });
 
 module.exports = router;
