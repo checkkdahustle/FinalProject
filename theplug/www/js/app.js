ffa01cb68c0591c8af2e4bb64c56fd773ca7e470
checkkdahustle <checkkdahustle@fullsail.edu>
2016-08-27 23:35:36 -0400

    rename app in module

--- theplug/www/js/app.js
+++ theplug/www/js/app.js
@@ -1,9 +1,9 @@
-// Ionic Starter App
+// Ionic theplugsview App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'theplugsview' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'firebase'])
+angular.module('theplugsview', ['ionic', 'firebase'])
 
 .run(function($ionicPlatform, $rootScope) {
   $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
