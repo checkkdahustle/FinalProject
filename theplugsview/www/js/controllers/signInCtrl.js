710fc4a717d90ede104dc300cad5b348c3660b6d
checkkdahustle <checkkdahustle@fullsail.edu>
2016-09-06 00:31:19 -0400

    proper comments were explaining the actions.

--- theplugsview/www/js/controllers/signInCtrl.js
+++ theplugsview/www/js/controllers/signInCtrl.js
@@ -6,28 +6,22 @@ angular.module('theplugsview').controller('SignInController', ['$scope', '$rootS
   Auth.$onAuthStateChanged(function(user) {
     if(user){
       $rootScope.currentUser = user;
+      // redirect to featured home page after logged in.
       console.log('signed in')
 			$state.go("tabs.home")
     } else {
       $rootScope.currentUser = null;
+      // redirect to sign in landing page
       console.log('signed out')
 			$state.go("landing")
     }
   })
-
+// login With Facebook
   $scope.loginWithFacebook = function () {
-    // console.log("HOLLA!")
     $scope.authObj.$signInWithPopup("facebook").then(function(result) {
-      // console.log("Signed in as:", result.user);
       $rootScope.currentUser = result.user;
     }).catch(function(error) {
       console.error("Authentication failed:", error);
     });
   }
-
-  $scope.debug = function () {
-    console.log("HOLLA!")
-  }
-
-
 }]);
