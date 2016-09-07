c44204f691d3eee8077796188788ae1e3ac0c325
checkkdahustle <checkkdahustle@fullsail.edu>
2016-09-07 00:01:29 -0400

    add a User factory module

--- theplugsview/www/js/services/AuthSvc.js
+++ theplugsview/www/js/services/AuthSvc.js
@@ -4,3 +4,25 @@ angular.module('theplugsview')
     return $firebaseAuth();
   }
 ]);
+
+
+angular.module('theplugsview')
+.factory("User", ["$firebaseArray",
+  function($firebaseArray) {
+    // create a new service based on $firebaseObject
+    var User = $firebaseObject.$extend({
+      // these methods exist on the prototype, so we can access the data using `this`
+      getFullName: function() {
+        return this.currentUser.displayName;
+      }
+    });
+
+    return function(userId) {
+      var userRef = firebase.database().ref()
+        .child("users").child(userId);
+
+      // create an instance of User (the new operator is required)
+      return new User(userRef);
+    }
+  }
+]);
