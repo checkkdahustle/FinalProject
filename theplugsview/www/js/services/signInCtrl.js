73437ef884618710e25ba5d38ccc7927856ea4f5
checkkdahustle <checkkdahustle@fullsail.edu>
2016-09-16 19:08:58 -0400

    added the app name and made code readable. Also add user data in console log and proper comments.

--- theplugsview/www/js/services/signInCtrl.js
+++ theplugsview/www/js/services/signInCtrl.js
@@ -1,26 +1,13 @@
-
-angular.module('theplugsview').controller('SignInController', ['$scope', '$rootScope', '$http', '$state', 'Auth', function($scope, $rootScope, $http, $state, Auth) {
-  console.log("Sign In View");
+theplugsview
+.controller('SignInController', [
+  '$scope',
+  '$rootScope',
+  '$state',
+  'Auth',
+  function($scope, $rootScope, $state, Auth) {
   $rootScope.authObj = Auth;
 
-  // Auth State Change - User signed in and out.
-  Auth.$onAuthStateChanged(function(user) {
-    if(user){
-      // var userRef = new Firebase('');
-      // var userObj = $firebaseObject(userRef);
-      // var userRef = firebase.database().ref().child('users').set().key;
-      $rootScope.currentUser = user;
-      // redirect to featured home page after logged in.
-      console.log('signed in')
-			$state.go("tabs.home")
-    } else {
-      $rootScope.currentUser = null;
-      // redirect to sign in landing page
-      console.log('signed out')
-			$state.go("landing")
-    }
-  })
-// login With Facebook
+  // login With Facebook
   $scope.loginWithFacebook = function () {
     $scope.authObj.$signInWithPopup("facebook").then(function(result) {
       $rootScope.currentUser = result.user;
@@ -28,4 +15,24 @@ angular.module('theplugsview').controller('SignInController', ['$scope', '$rootS
       console.error("Authentication failed:", error);
     });
   }
+
+
+  // Auth State Change - User signed in and out.
+  Auth.$onAuthStateChanged(function(user) {
+    if(user){
+      $rootScope.currentUser = user;
+      // redirect to featured home page after logged in.
+      console.log('signed in', user)
+      $state.go("tabs.home")
+      // result is suppose to push into database but doesn't
+      var userRef = firebase.database().ref('users/uid').push();
+      return userRef;
+
+    } else {
+      // redirect to sign in landing page
+      $rootScope.currentUser = null;
+      console.log('signed out', user)
+			$state.go("landing")
+    }
+  })
 }]);
