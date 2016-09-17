45d5e65e1c5f975909c94fac00abeb560a6502a2
checkkdahustle <checkkdahustle@fullsail.edu>
2016-09-17 19:10:43 -0400

    remove console log on line10,14,27 from features ctrl and add proper comments

--- theplugsview/www/js/controllers/features.js
+++ theplugsview/www/js/controllers/features.js
@@ -1,32 +1,38 @@
 theplugsview
-.controller('FeaturedController', ['$scope', '$http', '$state', '$firebaseArray', 'currentAuth', 'Auth', function($scope, $http, $state, $firebaseArray, currentAuth, Auth) {
+.controller('FeaturedController', [
+	'$scope',
+	'$http',
+	'$state',
+	'$firebaseArray',
+	'currentAuth',
+	'Auth', function($scope, $http, $state, $firebaseArray, currentAuth, Auth) {
 
-	console.log("Features View");
-
+	// retrive concert data from json file.
+	// Set all concerts info & each concert in a variable.
+	// hide delete and reorder button, for Admins only.
   $http.get('js/data.json').success(function(data) {
-    console.log(data);
       $scope.concerts = data.concerts;
       $scope.whichconcert = $state.params.cId;
       $scope.data = {
         showDelete:false, showReorder: false
       };
 
+			// Delete item from list, for Admin only.
       $scope.onItemDelete = function(item) {
         $scope.concerts.splice($scope.concerts.indexOf(item), 1);
       }
-
+			// on pull down, refresh page and retrive concert data.
       $scope.doRefresh =function() {
       $http.get('js/data.json').success(function(data) {
-				console.log( data);
 					$scope.concerts = data.concerts;
           $scope.$broadcast('scroll.refreshComplete');
         });
       }
-
+			// toggle selected list to show star an item.
       $scope.toggleStar = function(item) {
         item.star = !item.star;
       }
-
+			// Reorder concert list items, Admins only.
       $scope.moveItem = function (item, fromIndex, toIndex) {
         $scope.concerts.splice(fromIndex, 1);
         $scope.concerts.splice(toIndex, 0, item);
