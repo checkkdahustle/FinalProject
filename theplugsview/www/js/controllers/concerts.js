d2eb534a49e0c5d542d1236c10348e0e0f7c9ab0
checkkdahustle <checkkdahustle@fullsail.edu>
2016-09-17 19:22:53 -0400

    removed console log on line9 from concerts ctrl and add proper comments.

--- theplugsview/www/js/controllers/concerts.js
+++ theplugsview/www/js/controllers/concerts.js
@@ -5,30 +5,32 @@ theplugsview
   '$state' ,
    function($scope, $http, $state) {
 
-  $http.get('js/data.json').success(function(data) {
-    console.log(data);
+     // retrive concert data from json file.
+     $http.get('js/data.json').success(function(data) {
+      // Set all concerts info & each concert in a variable.
       $scope.concerts = data.concerts;
+      // hide delete and reorder button, for Admins only.
       $scope.whichconcert = $state.params.cId;
       $scope.data = {
         showDelete: false,
         showReorder: false
       };
-
+      // Delete item from list, for Admin only.
       $scope.onItemDelete = function(item) {
         $scope.concerts.splice($scope.concerts.indexOf(item), 1);
       }
-
+      // on refresh pull down, retrive concert data.
       $scope.doRefresh =function() {
       $http.get('js/data.json').success(function(data) {
           $scope.concerts = data.concerts;
           $scope.$broadcast('scroll.refreshComplete');
         });
       }
-
+      // toggle selected list to show star an item.
       $scope.toggleStar = function(item) {
         item.star = !item.star;
       }
-
+      // Reorder concert list items, Admins only.
       $scope.moveItem = function (item, fromIndex, toIndex) {
         $scope.concerts.splice(fromIndex, 1);
         $scope.concerts.splice(toIndex, 0, item);
