d6912a60f2dcf7b6f1a9cb51e8e031294af44ab1
checkkdahustle <checkkdahustle@fullsail.edu>
2016-09-16 21:30:13 -0400

    add the variable short cut name to file and added a constant along with a service for auth.

--- theplugsview/www/js/services/AuthSvc.js
+++ theplugsview/www/js/services/AuthSvc.js
@@ -1,28 +1,8 @@
 // Authentication for checking routes
-angular.module('theplugsview')
-.factory("Auth", ["$firebaseAuth", function($firebaseAuth) {
-    return $firebaseAuth();
-  }
-]);
-
-
-angular.module('theplugsview')
-.factory("User", ["$firebaseObject",
-  function($firebaseObject) {
-    // create a new service based on $firebaseObject
-    return $firebaseObject.$extend({
-      // these methods exist on the prototype, so we can access the data using `this`
-      getFullName: function() {
-        return this.first_name + " " + this.last_name;
-      }
-    });
-
-    return function(userId) {
-      var userRef = firebase.database().ref()
-        .child("users").child(userId);
-
-      // create an instance of User (the new operator is required)
-      return new User(userRef);
-    }
-  }
-]);
+theplugsview
+.constant('FirebaseUrl', 'https://the-plugs-view.firebaseio.com/')
+.service('rootRef',['FirebaseUrl', firebase])
+.factory("Auth", ["$firebaseAuth",
+  function Auth($firebaseAuth, rootRef) {
+    return $firebaseAuth(rootRef);
+  }])
